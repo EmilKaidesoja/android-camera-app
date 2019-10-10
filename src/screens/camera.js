857684229c98320ae0e18c820c06b33b5e745639
elmeri <elmeri.kinnunen@myy.haaga-helia.fi>
2019-10-10 15:53:48 +0300

    screens added

---
+++ src/screens/camera.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+class camera extends Component {
+  render() {
+    return {};
+  }
+}
+
+export default camera();
